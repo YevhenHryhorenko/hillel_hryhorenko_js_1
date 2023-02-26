d69d9cd17164dce59a9d536928e57d53cdbd0db3
YevhenHryhorenko <mrhryhorenko@gmail.com>
2023-02-26 15:36:00 +0200

    delete check

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 let userName;
 
-while (true) {
-    userName = prompt('Please, enter your name below:');
-    
-	if (userName == "John") {
-        alert(`Hello, ${userName}! How are you?`);
-        break;
-    } else {
-        alert('Your name is wrong, please, try again');
-	}
-}
+userName = prompt('Please, enter your name below:');
+alert(`Hello, ${userName}! How are you?`);
